d6914d832369bbb6529d50781ca50deacbce0cdd
Somojit Arambam <somoarambam123@gmail.com>
2024-10-05 12:56:15 +0530

    homepage layout changed

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { FlipWords } from './ui/flip-words'
 
 const Banner = () => {
-    const words = ["DURGA PUJA SALE 11 - 15 October","MAC COSMETIC","HUDA BEAUTY", "LA GIRL", "LN PRO", "KRYOLAN", "MILANI", "FOREVER 52"];
+    const words = ["UPTO 50 % OFF", "DURGA PUJA SALE", "5th October - 11th October Midnight"];
   return (
     <div className="w-full flex items-center justify-center h-12">
-                <div className="lg:w-[800px] w-[400px] h-8 rounded-2xl bg-gray-900 text-white font-medium flex items-center justify-center text-sm">
+                <div className="lg:w-[800px] w-[400px] h-8 rounded-2xl bg-gray-900 text-yellow-400 font-bold flex items-center justify-center text-sm">
             <FlipWords words={words} /> <br />
         </div>
 
